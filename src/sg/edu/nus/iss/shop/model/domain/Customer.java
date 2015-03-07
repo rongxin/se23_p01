d8b1a8fa4cec786cdd2fba6f2ddd4bfddca7141a
Zhu Bin <A0135867R@u.nus.edu>
2015-03-07 21:49:42 +0800

    Added Customer class skeleton 1. Added attribute ID 2. Added getter and setter methods 3. Added default contructor

--- src/sg/edu/nus/iss/shop/model/domain/Customer.java
+++ src/sg/edu/nus/iss/shop/model/domain/Customer.java
@@ -1,5 +1,18 @@
 package sg.edu.nus.iss.shop.model.domain;
 
 public class Customer {
+	private long id;
+
+	public Customer() {
+		this.id = 0;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
 
 }
