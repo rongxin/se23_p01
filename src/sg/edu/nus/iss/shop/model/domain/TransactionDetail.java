80c03da2a5e58fa4808a02b2b51c776691e21bf7
Oscar <oscar.castro@bcdtravelapac.com>
2015-03-07 20:58:01 +0800

    Adding Transaction and TransactionDetails

--- src/sg/edu/nus/iss/shop/model/domain/TransactionDetail.java
+++ src/sg/edu/nus/iss/shop/model/domain/TransactionDetail.java
@@ -1,5 +1,49 @@
 package sg.edu.nus.iss.shop.model.domain;
 
+/**
+ * 
+ * @author Oscar Castro Araya
+ * @version 1.0
+ * Class to store a single line of product in the invoice.
+ */
 public class TransactionDetail {
+	private Transaction transaction;
+	private Product product;
+	private int quantity;
+	
+	/**
+	 * Default Constructor
+	 * @param transaction
+	 * @param product
+	 * @param quantity
+	 */
+	public TransactionDetail(Transaction transaction, Product product, int quantity) {
+		this.transaction = transaction;
+		this.product = product;
+		this.quantity = quantity;
+	}
 
+	public Transaction getTransaction() {
+		return transaction;
+	}
+
+	public void setTransaction(Transaction transaction) {
+		this.transaction = transaction;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
 }
