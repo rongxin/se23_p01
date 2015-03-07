fab7cee1e797d4deee2f883ca0cb6142a96438cf
Zhu Bin <A0135867R@u.nus.edu>
2015-03-07 22:54:43 +0800

    format codes

--- src/sg/edu/nus/iss/shop/model/domain/NonMemberCustomer.java
+++ src/sg/edu/nus/iss/shop/model/domain/NonMemberCustomer.java
@@ -2,7 +2,8 @@ package sg.edu.nus.iss.shop.model.domain;
 
 public class NonMemberCustomer extends Customer {
 	private final static String NON_MEMBER_CUSTOMER_ID = "NON_MEMBER_CUSTOMER";
-	public NonMemberCustomer(){
+
+	public NonMemberCustomer() {
 		super(NonMemberCustomer.NON_MEMBER_CUSTOMER_ID);
 	}
 }
