51b97a8a98337fce8fd8dc5491b0173624b95c38
Robin Cher <robincher@gmail.com>
2015-03-08 15:31:51 +0800

    Update Category Manager and Category Models with abstract implementation

--- src/sg/edu/nus/iss/shop/model/domain/Category.java
+++ src/sg/edu/nus/iss/shop/model/domain/Category.java
@@ -1,16 +1,39 @@
+/**
+* @author  robincher
+*/
 package sg.edu.nus.iss.shop.model.domain;
+
+import java.util.List;
+
+import sg.edu.nus.iss.shop.model.domain.Vendor;
 
 public class Category {
 	private String code;
 	private String name;
+	private List<Vendor> vendorList;
 
 	/**
 	 * Constructor for Category
+	 * @param code category code
+	 * @param name category name
 	 */
-	public Category(String code, String name, int runningSequence) {
-
+	public Category(String code, String name) {
+		this.code = code;
+		this.name = name;
 	}
-
+	
+	/**
+	 * Constructor for Category with vendor list 
+	 * @param code category code
+	 * @param name category name
+	 * @param vendorList List of vendors for this category
+	 */
+	public Category(String code, String name , List<Vendor> vendorList) {
+		this.code = code;
+		this.name = name;
+		this.vendorList = vendorList;
+	}
+	
 	/**
 	 * Method to get Category Code
 	 * @return category code
@@ -30,8 +53,7 @@ public class Category {
 	/**
 	 * Method to get Category Name
 	 * 
-	 * @return category name
-	 *            
+	 * @return category name        
 	 * */
 	public String getName() {
 		return this.name;
@@ -41,9 +63,19 @@ public class Category {
 	 * Setter Method for Category Name
 	 * 
 	 * @param name category name to be set
-	
 	 * */
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	/**
+	 * Method to get Vendor list 
+	 * 
+	 * @return vendor list for this category      
+	 * */
+	public List<Vendor> getVendorList() {
+		return this.vendorList;
+	}
+
+	
 }
