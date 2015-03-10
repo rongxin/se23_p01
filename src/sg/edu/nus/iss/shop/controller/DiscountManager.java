28433f398e2d94c406a431b50a2e2db92e8c3c50
DancingOnAir <A0135925@u.nus.edu>
2015-03-10 23:27:13 +0800

    update discount & discountmanager

--- src/sg/edu/nus/iss/shop/controller/DiscountManager.java
+++ src/sg/edu/nus/iss/shop/controller/DiscountManager.java
@@ -1,5 +1,21 @@
+/**
+ * @author	taotong
+ */
 package sg.edu.nus.iss.shop.controller;
 
-public class DiscountManager {
+import sg.edu.nus.iss.shop.exception.ApplicationGUIException;
+import sg.edu.nus.iss.shop.model.domain.Discount;
 
+public class DiscountManager {
+	private static DiscountManager theOnlyDiscountManager;
+	private static final String INVALID_DATE_ERROR_MESSAGE = "Invalid date.";
+	
+	public static DiscountManager getTheOnlyDiscountManager() {
+		if (theOnlyDiscountManager == null){
+			DiscountManager.theOnlyDiscountManager = new DiscountManager();
+		}	
+		return theOnlyDiscountManager;
+	}
+	
+	
 }
--- src/sg/edu/nus/iss/shop/model/domain/PublicDiscount.java
+++ src/sg/edu/nus/iss/shop/model/domain/PublicDiscount.java
@@ -1,5 +1,12 @@
 package sg.edu.nus.iss.shop.model.domain;
 
 public class PublicDiscount extends Discount {
-
+	private static final String APPLICABLE_TO_MEMBER = "A";
+	
+	public PublicDiscount(String discountCode, String description,
+			String startDate, String discountInDays, int discountPercentage,
+			String applicableToMember) {
+		super(discountCode, description, startDate, discountInDays, discountPercentage,
+				APPLICABLE_TO_MEMBER);
+	}
 }
