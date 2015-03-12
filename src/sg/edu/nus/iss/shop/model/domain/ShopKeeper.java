33f921389894953b996053c7797a58cd69951aa3
Su <A0006524@u.nus.edu>
2015-03-12 20:23:58 +0800

    Store keeper model class. Controller ..coming soon.

--- src/sg/edu/nus/iss/shop/model/domain/ShopKeeper.java
+++ src/sg/edu/nus/iss/shop/model/domain/ShopKeeper.java
@@ -1,5 +1,29 @@
 package sg.edu.nus.iss.shop.model.domain;
 
 public class ShopKeeper {
+	
+	private String name;
+	private String password;
+	
+	public ShopKeeper(String name, String password) {
+		this.setName(name);
+		this.setPassword(password);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
