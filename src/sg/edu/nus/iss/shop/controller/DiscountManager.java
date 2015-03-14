41f1d83948418ea18993d0708f786f9bad8ceefe
Zhu Bin <A0135867R@u.nus.edu>
2015-03-14 14:04:41 +0800

    cleaned up imports

--- src/sg/edu/nus/iss/shop/controller/DiscountManager.java
+++ src/sg/edu/nus/iss/shop/controller/DiscountManager.java
@@ -3,16 +3,12 @@
  */
 package sg.edu.nus.iss.shop.controller;
 
-
-import java.time.LocalDate;
 import java.util.Iterator;
-import java.util.List;
 import java.util.LinkedList;
+import java.util.List;
 
-import sg.edu.nus.iss.shop.exception.ApplicationGUIException;
 import sg.edu.nus.iss.shop.model.domain.Customer;
 import sg.edu.nus.iss.shop.model.domain.Discount;
-import sg.edu.nus.iss.shop.model.domain.Member;
 
 /**
  * @author User
@@ -21,34 +17,34 @@ import sg.edu.nus.iss.shop.model.domain.Member;
 public class DiscountManager {
 	private static DiscountManager theOnlyDiscountManager;
 	private static final String INVALID_DATE_ERROR_MESSAGE = "Invalid date.";
-	
-	private DiscountManager(){
-		
+
+	private DiscountManager() {
+
 	}
-	
+
 	public static DiscountManager getDiscountManager() {
-		if (theOnlyDiscountManager == null){
+		if (theOnlyDiscountManager == null) {
 			DiscountManager.theOnlyDiscountManager = new DiscountManager();
-		}	
+		}
 		return theOnlyDiscountManager;
 	}
-	
+
 	public List<Discount> getAllDiscounts() {
 		return new LinkedList<Discount>();
 	}
-	
-	public Discount getMaxDiscount(Customer customer){
+
+	public Discount getMaxDiscount(Customer customer) {
 		Discount maxDiscount = null;
 		List<Discount> discountList = DiscountManager.getDiscountManager().getAllDiscounts();
 		Iterator<Discount> iter = discountList.iterator();
-		
-		while(iter.hasNext()){
+
+		while (iter.hasNext()) {
 			Discount discount = iter.next();
-			if(discount.isApplicable(customer)){
-				if (maxDiscount == null || discount.getDiscountPercentage() > maxDiscount.getDiscountPercentage()){
+			if (discount.isApplicable(customer)) {
+				if (maxDiscount == null || discount.getDiscountPercentage() > maxDiscount.getDiscountPercentage()) {
 					maxDiscount = discount;
 				}
-			}		
+			}
 		}
 
 		return maxDiscount;
