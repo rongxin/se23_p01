7a7ad62c4d748d8dc91f5ca7d97e63b15d8e7391
Zhu Bin <A0135867R@u.nus.edu>
2015-03-14 18:56:23 +0800

    catch exception thrown by getAllCategories method

--- test/sg/edu/nus/iss/shop/controller/VendorTest.java
+++ test/sg/edu/nus/iss/shop/controller/VendorTest.java
@@ -4,38 +4,42 @@ import java.util.List;
 import java.util.Random;
 
 import org.junit.Assert;
-
 import org.junit.Test;
 
 import sg.edu.nus.iss.shop.model.domain.Category;
 import sg.edu.nus.iss.shop.model.domain.Vendor;
 
 public class VendorTest {
-	
+
 	@Test
-	public void categoryLazyLoadTest(){
+	public void categoryLazyLoadTest() {
 		String vendorName = "ZhuBin" + new Random().nextLong();
 		Vendor vendor = new Vendor(vendorName, "Test Vendor");
-		List<Category> categories = CategoryManager.getCategoryManager().getAllCategories();
-		if (categories == null || categories.size() == 0){
+		List<Category> categories;
+		try {
+			categories = CategoryManager.getCategoryManager().getAllCategories();
+		} catch (Exception e) {
+			Assert.fail("Exception occurred when getting all categories");
+			return;
+		}
+		if (categories == null || categories.size() == 0) {
 			Assert.fail("Cannot find a category");
-			return ;
+			return;
 		}
-		try{
+		try {
 			vendor = VendorManager.getVendorManager().addVendor(vendor.getName(), vendor.getDescription(), categories);
-		}
-		catch(Exception e){
+		} catch (Exception e) {
 			Assert.fail("Failed to add a new vendor");
-			return ;
+			return;
 		}
-		if (vendor == null){
+		if (vendor == null) {
 			Assert.fail("Failed to return the new vendor");
-			return ;
+			return;
 		}
 		Vendor retrievedVendor = VendorManager.getVendorManager().getVendorByName(vendorName);
 		List<Category> vendorCategories = retrievedVendor.getCategories();
 		Assert.assertEquals(categories.size(), vendorCategories.size());
-		
+
 		categories.removeAll(vendorCategories);
 		Assert.assertEquals(categories.size(), 0);
 	}
