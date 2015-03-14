a9a01ced4630640f10cdbcc2d96033c3dc2d5c55
Xia Rongxin <alecplus@hotmail.com>
2015-03-14 15:53:34 +0800

    Refactored to extract image icon creation to be helper

---
+++ src/sg/edu/nus/iss/shop/ui/IconHelper.java
@@ -0,0 +1,12 @@
+package sg.edu.nus.iss.shop.ui;
+
+import javax.swing.ImageIcon;
+
+public class IconHelper {
+	private static final String ICONS_PATH = "/sg/edu/nus/iss/shop/ui/icons/";
+
+	public static ImageIcon createImageIcon(String imageName) {
+		ImageIcon imageIcon = new ImageIcon(IconHelper.class.getResource(ICONS_PATH + imageName));
+		return imageIcon;
+	}
+}
