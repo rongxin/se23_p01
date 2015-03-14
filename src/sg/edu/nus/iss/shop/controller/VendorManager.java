e9e66f4a4a4cbf9ef7be13bbf882006abb31b4eb
Zhu Bin <A0135867R@u.nus.edu>
2015-03-14 18:49:15 +0800

    catch exception thrown by getAllCategories() method

--- src/sg/edu/nus/iss/shop/controller/VendorManager.java
+++ src/sg/edu/nus/iss/shop/controller/VendorManager.java
@@ -18,7 +18,7 @@ public class VendorManager {
 	private static final String INVALID_DESCRIPTION_ERROR_MESSAGE = "Invalid vendor description";
 	private static final String NIL_CATEGORY_ERROR_MESSAGE = "At least one category is needed";
 	private static final String SAVE_VENDOR_FAIL_ERROR_MESSAGE = "Failed to save the vendor";
-	
+
 	private static VendorManager theOnlyVendorManager;
 
 	private VendorManager() {
@@ -32,12 +32,18 @@ public class VendorManager {
 		return VendorManager.theOnlyVendorManager;
 	}
 
-	/* return distinctive vendors for all categories***/
+	/* return distinctive vendors for all categories** */
 	public List<Vendor> getAllVendors() {
-		List<Category> categoryList = CategoryManager.getCategoryManager().getAllCategories();
+		List<Category> categoryList;
+		try {
+			categoryList = CategoryManager.getCategoryManager().getAllCategories();
+		} catch (Exception e) {
+			e.printStackTrace();
+			categoryList = new LinkedList<Category>();
+		}
 		List<Vendor> vendorList = new LinkedList<Vendor>();
 		Iterator<Category> it = categoryList.iterator();
-		while (it.hasNext()){
+		while (it.hasNext()) {
 			Category category = it.next();
 			List<Vendor> categoryVendorList = this.listVendorForCategory(category);
 			vendorList.removeAll(categoryVendorList);
@@ -47,73 +53,70 @@ public class VendorManager {
 	}
 
 	public List<Vendor> listVendorForCategory(Category category) {
-		if (category == null){
+		if (category == null) {
 			return new LinkedList<Vendor>();
 		}
 		List<Vendor> vendorList = new LinkedList<Vendor>();
 		List<Object> objectList;
-		try{
+		try {
 			objectList = PersistentService.getService().retrieveVendors(category);
-		}
-		catch(Exception e){
+		} catch (Exception e) {
 			return new LinkedList<Vendor>();
 		}
-		
+
 		Iterator<Object> it = objectList.iterator();
-		while (it.hasNext()){
-			Vendor vendor  = (Vendor)it.next();
+		while (it.hasNext()) {
+			Vendor vendor = (Vendor) it.next();
 			vendorList.add(vendor);
 		}
 		return vendorList;
 	}
-	
-	/** returned vendor with null categories**/
-	public Vendor getVendorByName(String name){
-		if (name == null || name.trim().length()==0){
+
+	/** returned vendor with null categories **/
+	public Vendor getVendorByName(String name) {
+		if (name == null || name.trim().length() == 0) {
 			return null;
 		}
 		List<Vendor> allVendors = this.getAllVendors();
 		Iterator<Vendor> it = allVendors.iterator();
-		while (it.hasNext()){
+		while (it.hasNext()) {
 			Vendor existingVendor = it.next();
-			if (existingVendor.getName().equals(name)){
+			if (existingVendor.getName().equals(name)) {
 				return existingVendor;
 			}
 		}
 		return null;
 	}
 
-	/** if the vendor already exists, new description will not be taken 
-	 *  for existing vendor, only new categories will be added
+	/**
+	 * if the vendor already exists, new description will not be taken for
+	 * existing vendor, only new categories will be added
 	 * **/
 	public Vendor addVendor(String name, String description, List<Category> categories) throws ApplicationGUIException {
-		if (name == null || name.trim().length()<VendorManager.VENDOR_NAME_MIN_LENGTH || name.trim().length()>VendorManager.VENDOR_NAME_MAX_LENGTH){
+		if (name == null || name.trim().length() < VendorManager.VENDOR_NAME_MIN_LENGTH || name.trim().length() > VendorManager.VENDOR_NAME_MAX_LENGTH) {
 			throw new ApplicationGUIException(VendorManager.INVALID_NAME_ERROR_MESSAGE);
 		}
-		if (description == null || description.trim().length()<VendorManager.VENDOR_DESCRIPTION_MIN_LENGTH || description.trim().length() >VendorManager.VENDOR_DESCRIPTION_MAX_LENGTH){
+		if (description == null || description.trim().length() < VendorManager.VENDOR_DESCRIPTION_MIN_LENGTH || description.trim().length() > VendorManager.VENDOR_DESCRIPTION_MAX_LENGTH) {
 			throw new ApplicationGUIException(VendorManager.INVALID_DESCRIPTION_ERROR_MESSAGE);
 		}
-		if (categories == null || categories.size()==0){
+		if (categories == null || categories.size() == 0) {
 			throw new ApplicationGUIException(VendorManager.NIL_CATEGORY_ERROR_MESSAGE);
 		}
 		Vendor vendor = this.getVendorByName(name);
-		if (vendor == null){ // new vendor
+		if (vendor == null) { // new vendor
 			vendor = new Vendor(name, description);
-			try{
+			try {
 				PersistentService.getService().saveRecord(vendor);
-			}
-			catch(Exception e){
+			} catch (Exception e) {
 				throw new ApplicationGUIException(VendorManager.SAVE_VENDOR_FAIL_ERROR_MESSAGE);
 			}
-		}
-		else {			// existing vendor
+		} else { // existing vendor
 			List<Category> existingCategories = vendor.getCategories();
 			categories.removeAll(existingCategories);
 			vendor.setCategories(categories);
-			try{
+			try {
 				PersistentService.getService().saveRecord(vendor);
-			}
-			catch(Exception e){
+			} catch (Exception e) {
 				throw new ApplicationGUIException(VendorManager.SAVE_VENDOR_FAIL_ERROR_MESSAGE);
 			}
 		}
