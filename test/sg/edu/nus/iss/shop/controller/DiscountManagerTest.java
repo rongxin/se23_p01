a031a0c5043ce047245d329dbfe12830f9b09d12
DacningOnAir <A0135925@u.nus.edu>
2015-03-18 13:44:51 +0800

    add test method into DiscountManagerTest

--- test/sg/edu/nus/iss/shop/controller/DiscountManagerTest.java
+++ test/sg/edu/nus/iss/shop/controller/DiscountManagerTest.java
@@ -1,25 +1,53 @@
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import sg.edu.nus.iss.shop.controller.DiscountManager;
+import sg.edu.nus.iss.shop.exception.ApplicationGUIException;
+import sg.edu.nus.iss.shop.model.domain.Discount;
+import sg.edu.nus.iss.shop.model.domain.Product;
 
-
-public class DiscountManagerTest extends TestCase{
+public class DiscountManagerTest extends TestCase {
 	private DiscountManager discountManager;
 	private Discount testDiscount;
-	
+	private Discount modifiedDiscount;
+
 	@Before
 	public void setup() {
-
+		testDiscount = Discount("01", "Christmas Day", 10, "2015-12-24", "3",
+				"A");
 	}
-	
+
 	@Test
-	public void testObjectInitialization(){
+	public void testObjectInitialization() {
 		discountManager = DiscountManager.getDiscountManager();
 		DiscountManager newDiscountManager = discountManager;
-		assertSame("Object should be the same",discountManager,newDiscountManager);
+		assertSame("Object should be the same", discountManager,
+				newDiscountManager);
 	}
-	
-	
+
+	@Test
+	public void testGetDiscountByCode() {
+		Discount compareDiscount = DiscountManager.getDiscountManager()
+				.getDiscountByCode("01");
+		assertNotNull(compareDiscount);
+		assertSame(testDiscount, compareDiscount);
+	}
+
+	@Test
+	public void testEditDiscount() {
+		modifiedDiscount = Disocunt("01", "National Day", 20, "2015-08-08",
+				"3", "A");
+		assertSame(
+				modifiedDiscount,
+				DiscountManager.getDiscountManager().editDiscount(
+						modifiedDiscount.getDiscountCode(),
+						modifiedDiscount.getDescription(),
+						modifiedDiscount.getDiscountPercentage(),
+						modifiedDiscount.getStartDate(),
+						modifiedDiscount.getDiscountInDays(),
+						modifiedDiscount.getApplicableToMember()));
+	}
 }
