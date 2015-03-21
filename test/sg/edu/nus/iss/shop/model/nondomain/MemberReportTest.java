cb77d614c12d3da09859439d90ab9c9b53d653d3
lokeshkb <lokbhaarthy@gmail.com>
2015-03-21 15:16:05 +0800

    Added Test package for not domain object

---
+++ test/sg/edu/nus/iss/shop/model/nondomain/MemberReportTest.java
@@ -0,0 +1,66 @@
+package sg.edu.nus.iss.shop.model.nondomain;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import sg.edu.nus.iss.shop.controller.MemberManager;
+import sg.edu.nus.iss.shop.exception.ApplicationGUIException;
+import sg.edu.nus.iss.shop.model.domain.Member;
+
+public class MemberReportTest {
+	
+	private MemberReport memberReport = null;
+	MemberManager memberManager = null;
+	
+	@Before
+	public void setup() {
+		memberReport = MemberReport.getMemberReport();
+		memberManager = MemberManager.getMemberManager();
+	}
+	
+	@Test
+	public void testMemberReportInitialization(){
+		assertNotNull("MemberReport Object should not be null", memberReport);
+		MemberReport newMemberReport = MemberReport.getMemberReport();
+		assertNotNull("MemberReport Object should not be null", memberReport);
+		assertSame("The MemberReport object doesn't match", newMemberReport, memberReport);
+	}
+	
+	@Test
+	public void testRetreiveAndGenerateReportData() throws ApplicationGUIException{
+		
+		List<String[]> memberReportList = null;
+		List<Member> memberList = null;
+		memberReportList = memberReport.retreiveAndGenerateReportData();
+		assertNotNull("The member report data should not be null", memberReportList);
+		if(memberReportList.size() > 0){
+			for(String[] memberArray : memberReportList){
+				assertNotNull("The member array should not be null!", memberArray);
+				boolean isInvalidaData = checkArrayHasNullOrEmpty(memberArray);
+				assertFalse("The value inside member array should not be null or empty", isInvalidaData == false);
+			}
+		}else{
+			memberList = memberManager.getAllMembers();	
+			assertFalse("The member report data is incorrect!", !memberList.isEmpty());
+		}
+	}
+	
+	
+	/**
+	 * Method to check whether the array got a invalid data
+	 * */
+	private boolean checkArrayHasNullOrEmpty(String[] memberArray){
+		
+		for(String memberDetails : memberArray){
+			if(memberDetails == null || memberDetails == ""){
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
