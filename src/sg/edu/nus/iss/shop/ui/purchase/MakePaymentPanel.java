cf973d3cfe531b049190dee6df5dad76b2802e14
Xia Rongxin <alecplus@hotmail.com>
2015-03-22 12:48:30 +0800

    Adjusted layout for payment panel

--- src/sg/edu/nus/iss/shop/ui/purchase/MakePaymentPanel.java
+++ src/sg/edu/nus/iss/shop/ui/purchase/MakePaymentPanel.java
@@ -4,6 +4,7 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -27,47 +28,87 @@ public class MakePaymentPanel extends JPanel {
 	}
 
 	private void initMakePaymentPanel() {
+		setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 
 		setLayout(new GridBagLayout());
-		setBorder(BorderFactory.createCompoundBorder(BorderFactory.createTitledBorder(" Make Payments "),
+		GridBagConstraints gc = new GridBagConstraints();
+
+		gc = LayoutHelper.createCellConstraint(0, 0);
+		this.add(createRedeemLoyaltyPointsPanel(), gc);
+
+		gc = LayoutHelper.createCellConstraint(0, 1);
+		this.add(new JPanel(), gc);
+
+		gc = LayoutHelper.createCellConstraint(0, 2);
+		this.add(createMakePaymentPanel(), gc);
+
+		gc = LayoutHelper.createCellConstraint(0, 3);
+		this.add(new JPanel(), gc);
+
+		gc = LayoutHelper.createCellConstraint(0, 4);
+		this.add(new JPanel(), gc);
+
+		gc = LayoutHelper.createCellConstraint(0, 5);
+		this.add(new JPanel(), gc);
+
+	}
+
+	private JPanel createRedeemLoyaltyPointsPanel() {
+		JPanel p = new JPanel();
+		p.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createTitledBorder(" Redeem Loyalty Points  "),
 				BorderFactory.createEmptyBorder(5, 5, 5, 5)));
+		p.setLayout(new GridBagLayout());
 		GridBagConstraints gc = new GridBagConstraints();
 
 		// column 1
 		gc = LayoutHelper.createCellConstraint(0, 0);
-		JLabel categoryCodeLabel = new JLabel("Loyaty  Points: ");
-		add(categoryCodeLabel, gc);
+		JLabel loyaltyPointsLabel = new JLabel("Loyaty  Points: ");
+		p.add(loyaltyPointsLabel, gc);
 
-		gc = LayoutHelper.createCellConstraint(0, 1);
-		JLabel categoryNameLabel = new JLabel("Paid Amount: ");
-		add(categoryNameLabel, gc);
-
-		gc = LayoutHelper.createCellConstraint(0, 3);
-		add(new JLabel(), gc);
-		gc = LayoutHelper.createCellConstraint(0, 4);
-		add(new JLabel(), gc);
-		gc = LayoutHelper.createCellConstraint(0, 5);
-		add(new JLabel(), gc);
-		gc = LayoutHelper.createCellConstraint(0, 6);
-		add(new JLabel(), gc);
-		gc = LayoutHelper.createCellConstraint(0, 7);
-		add(new JLabel(), gc);
-		gc = LayoutHelper.createCellConstraint(0, 8);
-		add(new JLabel(), gc);
-		gc = LayoutHelper.createCellConstraint(0, 9);
-		add(new JLabel(), gc);
+		gc = LayoutHelper.createCellConstraint(0, 0);
+		p.add(new JPanel(), gc);
 
 		// column 2
 		gc = LayoutHelper.createCellConstraint(1, 0);
-		loyatyPointsField = new JTextField(15);
-		loyatyPointsField.setToolTipText("Please input points to redeem.");
-		add(loyatyPointsField, gc);
+		loyatyPointsField = new JTextField(10);
+		p.add(loyatyPointsField, gc);
 
 		gc = LayoutHelper.createCellConstraint(1, 1);
-		paidAmountField = new JTextField(15);
-		paidAmountField.setToolTipText("Please input total amount paid");
-		add(paidAmountField, gc);
+		JButton redeemButton = new JButton("Redeem");
+		JPanel buttonPanel = new JPanel();
 
+		buttonPanel.add(redeemButton);
+		p.add(buttonPanel, gc);
+
+		return p;
+	}
+
+	private JPanel createMakePaymentPanel() {
+		JPanel p = new JPanel();
+		p.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createTitledBorder(" Payment  "),
+				BorderFactory.createEmptyBorder(5, 5, 5, 5)));
+		p.setLayout(new GridBagLayout());
+		GridBagConstraints gc = new GridBagConstraints();
+
+		// column 1
+		gc = LayoutHelper.createCellConstraint(0, 0);
+		JLabel amountToBePaidLabel = new JLabel("Amount to be  paid ($): ");
+		p.add(amountToBePaidLabel, gc);
+
+		gc = LayoutHelper.createCellConstraint(0, 1);
+		JLabel cashPaidAmount = new JLabel("Paid Amount ($): ");
+		p.add(cashPaidAmount, gc);
+
+		// column 2
+		gc = LayoutHelper.createCellConstraint(1, 0);
+		JLabel amountToBePaidValue = new JLabel("0.00");
+		p.add(amountToBePaidValue, gc);
+
+		gc = LayoutHelper.createCellConstraint(1, 1);
+		paidAmountField = new JTextField(10);
+		paidAmountField.setToolTipText("Please input  amount paid");
+		p.add(paidAmountField, gc);
+		return p;
 	}
 
 	public JTextField getLoyatyPointsField() {
