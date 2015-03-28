4658a89a8a82c149dca425a42fdf215f08c212bc
Oscar <oscar.castro@bcdtravelapac.com>
2015-03-28 16:14:12 +0800

    Printer

---
+++ test/sg/edu/nus/iss/shop/controller/PrinterManagerTest.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.shop.controller;
+
+import org.junit.Test;
+
+import sg.edu.nus.iss.shop.model.domain.Product;
+
+public class PrinterManagerTest {
+	
+	@Test
+	public void testPrintProduct() {
+		PrinterManager.getInstance().PrintProductBarCode(new Product(null, null, null, 0, 0, "6325135152", 0, 0));
+	}
+}
