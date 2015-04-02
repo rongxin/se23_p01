9c1c2e68f22bb469321a34a353cf39beb97d8c7f
Xia Rongxin <alecplus@hotmail.com>
2015-04-02 21:39:45 +0800

    added icons for vendor

--- src/sg/edu/nus/iss/shop/ui/category/ListCategoryPanel.java
+++ src/sg/edu/nus/iss/shop/ui/category/ListCategoryPanel.java
@@ -3,6 +3,7 @@ package sg.edu.nus.iss.shop.ui.category;
 /**
  * @author Xia Rongxin
  */
+import java.awt.Dimension;
 import java.util.List;
 
 import javax.swing.JButton;
@@ -14,6 +15,7 @@ import sg.edu.nus.iss.shop.model.domain.Category;
 import sg.edu.nus.iss.shop.ui.JTableButtonMouseListener;
 import sg.edu.nus.iss.shop.ui.JTableButtonRenderer;
 import sg.edu.nus.iss.shop.ui.main.ShopApplication;
+import sg.edu.nus.iss.shop.ui.util.IconHelper;
 
 public class ListCategoryPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
@@ -33,18 +35,23 @@ public class ListCategoryPanel extends JPanel {
 
 		tableModel = new CategoryTableModel();
 		for (Category category : categories) {
-			JButton vendorButton = new JButton("Vendors");
+			JButton vendorButton = new JButton(IconHelper.createImageIcon("vendor.png"));
 			vendorButton.addActionListener(new ViewCategoryVendorListener(shopApplication, category));
 			tableModel.addToTable(category, vendorButton);
 		}
 
 		JTable table = new JTable(tableModel);
-		table.getColumn("Action").setCellRenderer(new JTableButtonRenderer());
+		table.getColumn("Vendor").setCellRenderer(new JTableButtonRenderer());
 		table.addMouseListener(new JTableButtonMouseListener(table));
 		table.setName("Items");
 		table.setEnabled(true);
 		table.setAutoResizeMode(JTable.AUTO_RESIZE_SUBSEQUENT_COLUMNS);
+		// table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		table.getColumnModel().getColumn(0).setPreferredWidth(150);
+		table.getColumnModel().getColumn(1).setPreferredWidth(250);
+		table.getColumnModel().getColumn(2).setPreferredWidth(100);
 		JScrollPane scrollPane = new JScrollPane(table);
+		scrollPane.setPreferredSize(new Dimension(500, 450));
 		p.add(scrollPane);
 		return p;
 	}
